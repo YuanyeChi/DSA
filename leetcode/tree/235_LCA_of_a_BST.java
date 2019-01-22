3fb30d503083fa3a01193bb8e760b5a22f8cfee6
tsiye <37322570+tsiye@users.noreply.github.com>
2019-01-22 22:00:47 +0800

    1.22

---
+++ leetcode/tree/235_LCA_of_a_BST.java
@@ -0,0 +1,19 @@
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+	According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+O(N)
+	class Solution {
+		public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+			int parentVal = root.val;
+			int pVal = p.val;
+			int qVal = q.val;
+
+			if (pVal > parentVal && qVal > parentVal) 
+				return lowestCommonAncestor(root.right, p, q);
+			else if (pVal < parentVal && qVal < parentVal)
+				return lowestCommonAncestor(root.left, p, q);
+			else 
+				return root;
+		}
+	}
