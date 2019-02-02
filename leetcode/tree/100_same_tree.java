adf40559025bc0e37dd13a4f2c8cf384fccc67e8
tsiye <37322570+tsiye@users.noreply.github.com>
2019-02-02 12:41:19 +0800

    updates

--- leetcode/tree/100_same_tree.java
+++ leetcode/tree/100_same_tree.java
@@ -1,3 +1,5 @@
+//Given two binary trees, write a function to check if they are the same or not.
+//Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
 class Solution {
 	public boolean isSameTree(TreeNode p, TreeNode q) {
 		if (p == null && q == null) return true; //java和c的null不一样
