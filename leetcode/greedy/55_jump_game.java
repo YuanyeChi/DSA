61dcaa19cb72b4107fb724e11daebdba9ee6e326
tsiye <37322570+tsiye@users.noreply.github.com>
2019-04-08 20:49:49 +0800

    4.8

--- leetcode/greedy/55_jump_game.java
+++ leetcode/greedy/55_jump_game.java
@@ -1,5 +1,30 @@
+/*55. Jump Game
+Medium
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.*/
 
 //backtracking
+/*
+Time complexity : O(2^n). There are 2^n (upper bound) ways of jumping from the first position to the last, where nn is the length of array nums. For a complete proof, please refer to Appendix A.
+
+Space complexity : O(n). Recursion requires additional memory for the stack frames. 
+*/
 class Solution {
 	public boolean canJump(int[] nums) {
 		return canJump1(nums, 0);
@@ -15,3 +40,59 @@ class Solution {
 		return false;
 	}
 }
+
+//DP
+/*Time complexity : O(n^2). For every element in the array, say i, we are looking at the next nums[i] elements to its right aiming to find a GOOD index. nums[i] can be at most n, where n is the length of array nums.
+
+Space complexity : O(2n) = O(n). First n originates from recursion. Second n comes from the usage of the memo table. */
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
+public class Solution {
+    Index[] memo;
+
+    public boolean canJumpFromPosition(int position, int[] nums) {
+        if (memo[position] != Index.UNKNOWN) {
+            return memo[position] == Index.GOOD ? true : false;
+        }
+
+        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+            if (canJumpFromPosition(nextPosition, nums)) {
+                memo[position] = Index.GOOD;
+                return true;
+            }
+        }
+
+        memo[position] = Index.BAD;
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+        return canJumpFromPosition(0, nums);
+    }
+}
+
+//greedy
+//Time complexity : O(n). We are doing a single pass through the nums array, hence n steps, where n is the length of array nums.
+//Space complexity : O(1). We are not using any extra memory.
+Space complexity : O(1)O(1). We are not using any extra memory.
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
+
+
---
+++ leetcode/tree/102_binary_tree_level_order_traversal.java
@@ -0,0 +1,70 @@
+/*102. Binary Tree Level Order Traversal
+Medium
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//BFS
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        
+        if(root == null) return res;
+        
+        q.offer(root);
+        while(!q.isEmpty()){
+            int len = q.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i = 0; i < len; i++) {
+                if(q.peek().left != null) q.offer(q.peek().left);
+                if(q.peek().right != null) q.offer(q.peek().right);
+                subList.add(q.poll().val);
+            }
+            res.add(subList);
+        }
+        return res;
+    }
+}
+
+//DFS
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        levelHelper(res, root, 0);
+        return res;
+    }
+    
+    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= res.size()) {
+            res.add(new LinkedList<Integer>());
+        }
+        res.get(height).add(root.val);
+        levelHelper(res, root.left, height+1);
+        levelHelper(res, root.right, height+1);
+    }
+}
+
+
