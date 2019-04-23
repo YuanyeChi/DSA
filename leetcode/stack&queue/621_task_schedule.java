204827358fe606c6be1aa7c3c9bb2649dc97a936
tsiye <37322570+tsiye@users.noreply.github.com>
2019-04-24 07:12:53 +0800

    add problem conditions

--- leetcode/stack&queue/621_task_schedule.java
+++ leetcode/stack&queue/621_task_schedule.java
@@ -1,8 +1,25 @@
-//Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks. Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
+/*621. Task Scheduler
+Medium
 
-//However, there is a non-negative cooling interval n that means between two same tasks, there must be at least n intervals that CPU are doing different tasks or just be idle.
+Given a char array representing tasks CPU need to do. It contains capital letters A to Z where different letters represent different tasks. Tasks could be done without original order. Each task could be done in one interval. For each interval, CPU could finish one task or just be idle.
 
-//You need to return the least number of intervals the CPU will take to finish all the given tasks.
+However, there is a non-negative cooling interval n that means between two same tasks, there must be at least n intervals that CPU are doing different tasks or just be idle.
+
+You need to return the least number of intervals the CPU will take to finish all the given tasks.
+
+ 
+
+Example:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+Output: 8
+Explanation: A -> B -> idle -> A -> B -> idle -> A -> B.
+ 
+
+Note:
+
+The number of tasks is in the range [1, 10000].
+The integer n is in the range [0, 100].*/
 //Solution1 Priority Queue 
 class Solution {
 	public int leastInterval(char[] tasks, int n) {
